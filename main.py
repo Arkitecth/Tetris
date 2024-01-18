c121c6b139e35bf8c6d6f99491795d71049f1cfb
Trent_T <trenttuedon2000@gmail.com>
2024-01-18 18:12:58 -0500

    Created a Tetronimoe class

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ import pygame
 
 # pygame setup
 
+WHITE = (200, 200, 200)
+
+
+class Tetronimoe: 
+    def __init__(self) -> None:
+        self.x = 0 
+        
+
 
 class Game:
     def __init__(self, width, height) -> None:
         pygame.init()
+        self.width = width
+        self.height = height
         self.screen = pygame.display.set_mode((width, height))
         self.screen.fill("black")
         self.running = True
@@ -13,12 +23,20 @@ class Game:
 
     def start(self):
         while self.running:
+            self.draw_grid()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-            self.screen.fill("black")
+            # self.screen.fill("black")
             pygame.display.flip()
             self.clock.tick(60)
+
+    def draw_grid(self):
+        blockSize = 20
+        for x in range(0, self.width, blockSize):
+            for y in range(0, self.height, blockSize):
+                rect = pygame.Rect(x, y,`` blockSize, blockSize)
+                pygame.draw.rect(self.screen, WHITE, rect, 1)
 
 
 def main():
