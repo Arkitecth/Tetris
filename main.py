f1f1d4230014079175388b9596ea0b3e0ef17823
Trent_T <trenttuedon2000@gmail.com>
2024-01-20 14:31:50 -0500

    Added square to project

--- main.py
+++ main.py
@@ -5,10 +5,25 @@ import pygame
 WHITE = (200, 200, 200)
 
 
-class Tetronimoe: 
-    def __init__(self) -> None:
-        self.x = 0 
-        
+class Position:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+
+
+class Square:
+    def __init__(self, pos, win) -> None:
+        self.pos = pos
+        self.color = "blue"
+        self.win = win
+
+    def move(self, rect, pos):
+        rect.move_ip(pos)
+
+    def draw(self):
+        rect = pygame.draw.rect(self.win, self.color,
+                                pygame.Rect(400, 10, 60, 60))
+        return rect
 
 
 class Game:
@@ -22,8 +37,11 @@ class Game:
         self.clock = pygame.time.Clock()
 
     def start(self):
+        tetronimoe = Square((0, 0), self.screen)
         while self.running:
             self.draw_grid()
+            piece = tetronimoe.draw()
+            tetronimoe.move(piece, (500, 500))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
@@ -35,7 +53,7 @@ class Game:
         blockSize = 20
         for x in range(0, self.width, blockSize):
             for y in range(0, self.height, blockSize):
-                rect = pygame.Rect(x, y,`` blockSize, blockSize)
+                rect = pygame.Rect(x, y, blockSize, blockSize)
                 pygame.draw.rect(self.screen, WHITE, rect, 1)
 
 
